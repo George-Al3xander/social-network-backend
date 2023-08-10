c0685b172d60416bed15cabdbddcb55998002c05
George <george.valuiskyi@gmail.com>
2023-08-10 17:19:34 +0300

    Added feed route

--- controllers/controllerPost.js
+++ controllers/controllerPost.js
@@ -2,6 +2,7 @@ const Post = require("../models/modelPost")
 const Comment = require("../models/modelComment")
 const Like = require("../models/modelLike")
 const User = require("../models/modelUser")
+const Friendship = require("../models/modelFriendship.js")
 
 const getSortedPost = (id) =>  {
     let finPost = {};
@@ -51,6 +52,40 @@ const get_all = (req,res) => {
     }
 }
 
+//                      const comments = await Comment.find({postId: post._id});
+//                     const likes = await Like.find({postId: post._id});
+//                     return {...post,comments, likes}
+
+const get_feed = (req, res) => {
+    if(req.user) {
+       Friendship.find({status: true, participants: {$in: [req.user._id]}})
+       .then(async (relations) => {            
+            relations = await relations.map(async (rel) => {
+                let posts = await Post.find();
+                posts = posts.filter((post) => {
+                    return rel.participants.includes(post.user.id)
+                })   
+                return posts
+            })                        
+            relations = (await Promise.all(relations)).flat();
+            relations = relations.map(async (post) => {
+                post = post._doc;              
+                const comments = await Comment.find({postId: post._id});
+                const likes = await Like.find({postId: post._id});
+                
+                return {...post,comments, likes}
+                
+            })
+            relations = (await Promise.all(relations)).flat();
+
+            //console.log(relations)
+            res.json({data: relations})
+       })
+    } else {
+        res.status(403).json({msg: "No user signed"})
+    }
+}
+
 const create_post = (req , res) => {    
     if(req.user) {
         const valid = new RegExp(/\S/);
@@ -59,7 +94,7 @@ const create_post = (req , res) => {
                 throw "Text can't be empy"
             }             
             new Post({user: {id: req.user._id, name: req.user.name, avatar : req.user.avatar ? req.user.avatar : undefined}, text: req.body.text}).save()
-            .then(() => res.status(200).json({msg: "Post created"}))
+            .then(() => res.redirect(process.env.CLIENT_URI))
             .catch(() => res.status(400).json({msg: "Error"}))
         } catch (error) {
             res.status(403).json({msg: error})
@@ -107,5 +142,6 @@ module.exports = {
     get_all,
     create_post,
     get_one,
-    delete_post
+    delete_post,
+    get_feed
 }
--- routes/posts.js
+++ routes/posts.js
@@ -4,6 +4,7 @@ const controller = require("../controllers/controllerPost")
 
 
 router.get("/", controller.get_all);
+router.get("/feed", controller.get_feed);
 router.post("/", controller.create_post);
 router.get("/:id", controller.get_one);
 router.delete("/:id", controller.delete_post);
