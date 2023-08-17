93438e1c3c933fe612035f55a510e1575baacbb1
Heorhii <125189346+George-Al3xander@users.noreply.github.com>
2023-08-17 10:11:50 +0300

    Changed cors origin back to true

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ mongoose.connect(db_uri , {useNewUrlParser: true, useUnifiedTopology: true})
 .catch((err) => {console.log(err)});
 
 app.use(cors({
-  origin: "https://george-al3xander.github.io",  
+  origin: true,  
   credentials: true,
 }))
 app.use(cookieSession({
