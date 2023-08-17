c9ec58a142ea5168823e6c13e9af6d1e006af73f
George <george.valuiskyi@gmail.com>
2023-08-17 11:36:26 +0300

    Set the header right way

--- controllers/controllerAuth.js
+++ controllers/controllerAuth.js
@@ -12,9 +12,8 @@ const login_failed = (req,res) => {
 const login_success = (req,res) => {
     if(req.user) {        
             User.findById(req.user._id)
-            .then((user) => {
-                res.header("Access-Control-Allow-Origin", "*")
-                res.status(200).json({
+            .then((user) => {               
+                res.header("Access-Control-Allow-Origin", "*").status(200).json({
                     success: true,
                     message: "Loged in",
                     user,
